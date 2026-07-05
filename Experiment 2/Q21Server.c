ed8747e436061a9e63ef35bfd28b0949393302e1
Nightingales21 <nitin.harish21@gmail.com>
2026-07-05 15:14:17 +0530

    functions

--- Experiment 2/Q21Server.c
+++ Experiment 2/Q21Server.c
@@ -6,26 +6,8 @@
 
 struct sockaddr_in server_addr;
 
-
-int main()
+void *handle_client (int client_fd, char buffer[1024]) 
 {
-    int server_fd, client_fd;
-    char buffer[1024] = {0};
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-
-    server_addr.sin_port = htons(8080);
-
-    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-
-    listen(server_fd, 5);
-
-    printf("Server is waiting:");
-
-    client_fd = accept(server_fd, NULL, NULL); 
-
     read(client_fd, buffer, sizeof(buffer));
     
     FILE *file;
@@ -50,7 +32,40 @@ int main()
         
     }
 
-        fclose(file);
+    fclose(file);
+    retun NULL;
+}
+
+int main()
+{
+    int server_fd, client_fd;
+    char buffer[1024] = {0};
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    server_addr.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    listen(server_fd, 5);
+
+    printf("Server is waiting:");
+
+    while(1)
+    {
+        int *client_fd = malloc(sizeof(int));
+        client_fd = accept(server_fd, NULL, NULL);
+
+        pthread_create(&thread_id, NULL, handle_client(client_fd, buffer), (void*)new_sock);
+    
+        pthread_detach(thread_id);
+    }
+
+}
+
+    
 
     close(client_fd);
     close(server_fd);
